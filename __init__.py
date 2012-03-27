700d9d8591452e1b14165cd50784978d9628a57f
Matthew Johnson <mattjj@csail.mit.edu>
2012-03-27 17:56:37 -0400

    deleted a mistaken include in the init.py file

--- __init__.py
+++ __init__.py
@@ -3,6 +3,3 @@ from models import hsmm, hmm, hmm_sticky
 from distributions import observations, durations
 from internals.states import use_eigen
 
-# TODO make this better
-from plugins.subhmms.models import hsmm_subhmms
-
