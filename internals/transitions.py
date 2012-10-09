d55f8c4ffe33429c0116946cbbed4af1eb5668e5
Matthew Johnson <mattjj@csail.mit.edu>
2012-10-09 14:24:01 -0400

    cleaning done!

--- internals/transitions.py
+++ internals/transitions.py
@@ -25,34 +25,35 @@ class HDPHMMTransitions(object):
             self.beta = beta
 
     def resample(self,states_list=[]):
-        data = self._count_transitions(states_list)
-        m = self._get_m(data)
+        if len(states_lis) > 0:
+            trans_counts = self._count_transitions(states_list)
+            m = self._get_m(trans_counts)
+        else:
+            trans_counts = np.zeros((self.state_dim,self.state_dim),dtype=np.int32)
+            m = np.zeros((self.state_dim,self.state_dim),dtype=np.int32)
 
         self._resample_beta(m)
-        self._resample_A(data)
+        self._resample_A(trans_counts)
 
     def _resample_beta(self,m):
-        self.beta = stats.gamma.rvs(self.gamma / self.state_dim + np.sum(m,axis=0))
-        self.beta /= np.sum(self.beta)
-        assert not np.isnan(self.beta).any()
+        self.beta = np.random.dirichlet(self.gamma / self.state_dim + m.sum(0))
 
-    def _resample_A(self,data):
-        self.A = stats.gamma.rvs(self.alpha * self.beta + data)
-        self.A /= np.sum(self.A,axis=1)[:,na]
-        assert not np.isnan(self.A).any()
+    def _resample_A(self,trans_counts):
+        self.A = stats.gamma.rvs(self.alpha * self.beta + trans_counts)
+        self.A /= self.A.sum(1)[:,na]
 
     def _count_transitions(self,states_list):
-        data = np.zeros((self.state_dim,self.state_dim),dtype=np.int32)
+        trans_counts = np.zeros((self.state_dim,self.state_dim),dtype=np.int32)
         for states in states_list:
             if len(states) >= 2:
                 for idx in xrange(len(states)-1):
-                    data[states[idx],states[idx+1]] += 1
-        return data
+                    trans_counts[states[idx],states[idx+1]] += 1
+        return trans_counts
 
-    def _get_m(self,data):
+    def _get_m(self,trans_counts):
         m = np.zeros((self.state_dim,self.state_dim),dtype=np.int32)
-        if not (0 == data).all():
-            for (rowidx, colidx), val in np.ndenumerate(data):
+        if not (0 == trans_counts).all():
+            for (rowidx, colidx), val in np.ndenumerate(trans_counts):
                 m[rowidx,colidx] = (np.random.rand(val) < self.alpha * self.beta[colidx]\
                         /(np.arange(val) + self.alpha*self.beta[colidx])).sum()
         return m
@@ -64,17 +65,17 @@ class HDPHMMTransitionsConcResampling(HDPHMMTransitions):
 
 class LTRHDPHMMTransitions(HDPHMMTransitions):
     def _count_transitions(self,states_list):
-        data = super(LTRHDPHMMTransitions,self)._count_transitions(states_list)
-        assert (0==np.tril(data,-1)).all()
-        totalfrom = data.sum(1)
+        trans_counts = super(LTRHDPHMMTransitions,self)._count_transitions(states_list)
+        assert (0==np.tril(trans_counts,-1)).all()
+        totalfrom = trans_counts.sum(1)
         totalweight = np.triu(self.fullA).sum(1)
-        for i in range(data.shape[0]):
+        for i in range(trans_counts.shape[0]):
             tot = np.random.geometric(totalweight[i],size=totalfrom[i]).sum()
-            data[i,:i] = np.random.multinomial(tot,self.fullA[i,:i]/self.fullA[i,:i].sum())
-        return data
+            trans_counts[i,:i] = np.random.multinomial(tot,self.fullA[i,:i]/self.fullA[i,:i].sum())
+        return trans_counts
 
-    def _resample_A(self,data):
-        super(LTRHDPHMMTransitions,self)._resample_A(data)
+    def _resample_A(self,trans_counts):
+        super(LTRHDPHMMTransitions,self)._resample_A(trans_counts)
         self.fullA = self.A
         self.A = np.triu(self.A) / np.triu(self.A).sum(1)[:,na]
 
@@ -82,18 +83,18 @@ class LTRHDPHMMTransitions(HDPHMMTransitions):
 #  HDP-HSMM classes  #
 ######################
 
-class HDPHSMMTransitions(object):
+class HDPHSMMTransitions(HDPHMMTransitions):
     '''
     HDPHSMM transition distribution class.
     Uses a weak-limit HDP prior. Zeroed diagonal to forbid self-transitions.
 
-    Hyperparameters follow the notation in Fox et al., except the definitions of
-    alpha and gamma are reversed for no reason.
-        alpha, gamma
+    Hyperparameters follow the notation in Fox et al.
+        gamma: concentration paramter for beta
+        alpha: total mass concentration parameter for each row of trans matrix
 
     Parameters are the shared transition vector beta, the full transition matrix,
     and the matrix with the diagonal zeroed.
-        beta, A
+        beta, A, fullA
     '''
 
     def __init__(self,state_dim,alpha,gamma,beta=None,A=None,fullA=None):
@@ -108,44 +109,29 @@ class HDPHSMMTransitions(object):
             self.fullA = fullA
 
     def resample(self,stateseqs=[]):
-        states_noreps = map(operator.itemgetter(0),map(rle, stateseqs))
-
-        if not any(len(states_norep) >= 2 for states_norep in states_noreps):
-            # if there is no data we just sample from the prior
-            self.beta = np.random.dirichlet((self.gamma / self.state_dim)*np.ones(self.state_dim))
-
-            self.fullA = np.random.dirichlet(self.beta*self.alpha,size=self.state_dim)
-            self.A = (1.-np.eye(self.state_dim)) * self.fullA
-            self.A /= self.A.sum(1)[:,na]
-
-            assert not np.isnan(self.beta).any()
-            assert not np.isnan(self.fullA).any()
-            assert (self.A.diagonal() == 0).all()
+        if len(stateseqs) > 0:
+            states_noreps = map(operator.itemgetter(0),map(rle, stateseqs))
+            trans_counts = self._count_transitions(states_noreps)
+            augmented_data = self.augmented_data = self._augment_data(trans_counts)
+            m = self.m = self._get_m(augmented_data)
         else:
-            data = np.zeros((self.state_dim,self.state_dim))
-            for states_norep in states_noreps:
-                for idx in xrange(len(states_norep)-1):
-                    data[states_norep[idx],states_norep[idx+1]] += 1
-            assert (data.diagonal() == 0).all()
+            augmented_data = np.zeros((self.state_dim,self.state_dim),dtype=np.int32)
+            m = np.zeros((self.state_dim,self.state_dim),dtype=np.int32)
 
-            froms = np.sum(data,axis=1)
-            self_transitions = [np.random.geometric(1-pi_ii,size=n).sum() if n > 0 else 0
-                    for pi_ii,n in zip(self.fullA.diagonal(),froms)]
-            augmented_data = data + np.diag(self_transitions)
-            self.augmented_data = augmented_data # save it for possible use in child classes
+        self._resample_beta(m)
+        self._resample_A(augmented_data)
 
-            m = np.zeros((self.state_dim,self.state_dim))
-            for (i,j), n in np.ndenumerate(augmented_data):
-                m[i,j] = (np.random.rand(n) < self.alpha*self.beta[j] \
-                        / (np.arange(n) + self.alpha*self.beta[j])).sum()
-            self.m = m # save it for possible use in child classes
+    def _augment_data(self,trans_counts):
+        froms = trans_counts.sum(1)
+        self_transitions = [np.random.geometric(1-pi_ii,size=n).sum() if n > 0 else 0
+                for pi_ii,n in zip(self.fullA.diagonal(),froms)]
+        return trans_counts + np.diag(self_transitions)
 
-            self.beta = np.random.dirichlet(self.gamma/self.state_dim + m.sum(0))
-            self.fullA = np.random.gamma(self.alpha * self.beta + augmented_data)
-            self.fullA /= self.fullA.sum(1)[:,na]
-            self.A = self.fullA * (1.-np.eye(self.state_dim))
-            self.A /= self.A.sum(1)[:,na]
-            assert not np.isnan(self.A).any()
+    def _resample_A(self,augmented_data):
+        super(HDPHSMMTransitions,self)._resample_A(augmented_data)
+        self.fullA = self.A.copy()
+        self.A.flat[::self.A.shape[0]+1] = 0
+        self.A /= self.A.sum(1)[:,na]
 
 
 class HDPHSMMTransitionsConcResampling(HDPHSMMTransitions):
@@ -165,9 +151,9 @@ class HDPHSMMTransitionsConcResampling(HDPHSMMTransitions):
         self.augmented_data = []
         self.m = []
         super(HDPHSMMTransitionsConcResampling,self).resample(*args,**kwargs)
-        self.alpha_obj.resample(self.augmented_data,weighted_cols=self.beta,niter=5)
+        self.alpha_obj.resample(self.augmented_data,weighted_cols=self.beta,niter=10)
         self.alpha = self.alpha_obj.concentration*self.state_dim
-        self.gamma_obj.resample(self.m,niter=5)
+        self.gamma_obj.resample(self.m,niter=10)
         self.gamma = self.gamma_obj.concentration*self.state_dim
 
 
