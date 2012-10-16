17a9617c606bbc51ef1876214712fae85a43ba81
Matthew Johnson <mattjj@csail.mit.edu>
2012-10-15 20:37:47 -0400

    fixed imports

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 from matplotlib import cm
 from warnings import warn
 
-from basic.abstractions import ModelGibbsSampling
-from internals import states, initial_state, transitions
+from pyhsmm.basic.abstractions import ModelGibbsSampling
+from pyhsmm.internals import states, initial_state, transitions
 
 class HMM(ModelGibbsSampling):
     '''
@@ -259,12 +259,12 @@ class HSMM(HMM, ModelGibbsSampling):
 
     ### parallel sampling
     def add_data_parallel(self,data_id):
-        import parallel
+        from pyhsmm import parallel
         self.add_data(parallel.alldata[data_id])
         self.states_list[-1].data_id = data_id
 
     def resample_model_parallel(self,numtoresample='all'):
-        import parallel
+        from pyhsmm import parallel
         if numtoresample == 'all':
             numtoresample = len(self.states_list)
         elif numtoresample == 'engines':
@@ -285,14 +285,14 @@ class HSMM(HMM, ModelGibbsSampling):
         self._build_states_parallel(states_to_resample)
 
     def _push_self_parallel(self,states_to_resample):
-        import parallel
+        from pyhsmm import parallel
         states_to_restore = [s for s in self.states_list if s not in states_to_resample]
         self.states_list = []
         parallel.dv.push({'global_model':self},block=True)
         self.states_list = states_to_restore
 
     def _build_states_parallel(self,states_to_resample):
-        import parallel
+        from pyhsmm import parallel
         raw_stateseq_tuples = parallel.build_states.map([s.data_id for s in states_to_resample])
         for data_id, stateseq in raw_stateseq_tuples:
             self.add_data(
@@ -363,12 +363,12 @@ class HSMMPossibleChangepoints(HSMM, ModelGibbsSampling):
                     **kwargs))
 
     def add_data_parallel(self,data_id,**kwargs):
-        import parallel
+        from pyhsmm import parallel
         self.add_data(parallel.alldata[data_id],parallel.allchangepoints[data_id],**kwargs)
         self.states_list[-1].data_id = data_id
 
     def _build_states_parallel(self,states_to_resample):
-        import parallel
+        from pyhsmm import parallel
         raw_stateseq_tuples = parallel.build_states_changepoints.map([s.data_id for s in states_to_resample])
         for data_id, stateseq in raw_stateseq_tuples:
             self.add_data(
