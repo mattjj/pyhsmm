a71a7f7de6c3bd3eda8c9816cef4e40383dd8334
Matthew Johnson <mattjj@csail.mit.edu>
2013-05-09 16:44:10 -0400

    added example

---
+++ examples/library-mm.py
@@ -0,0 +1,62 @@
+from __future__ import division
+import numpy as np
+
+from pyhsmm.util.general import rle
+
+import pyhsmm
+from pyhsmm import distributions as d
+from pyhsmm.basic.pybasicbayes.models import FrozenMixtureDistribution
+
+###############
+#  load data  #
+###############
+
+# dummy parameters, not used
+
+f = np.load('/Users/mattjj/Dropbox/Test Data/TMT_50p_mixtures_and_data.npz')
+mus = f['mu']
+sigmas = f['sigma']
+data = f['data']
+
+library_size, obs_dim = mus.shape
+labels = f['labels']
+
+for i in range(sigmas.shape[0]):
+    sigmas[i] += np.eye(obs_dim)
+
+#####################################
+#  build observation distributions  #
+#####################################
+
+component_library = [d.Gaussian(
+    mu=mu,sigma=sigma,
+    mu_0=np.zeros(obs_dim),sigma_0=np.eye(obs_dim),nu_0=obs_dim+10,kappa_0=1., # dummies, not used
+    ) for mu,sigma in zip(mus,sigmas)]
+
+hsmm_obs_distns = [FrozenMixtureDistribution(
+    alpha_0=3.,
+    components=component_library,
+    weights=indicator,
+    ) for indicator in np.eye(library_size)]
+
+dur_distns = [d.NegativeBinomialIntegerRVariantDuration(np.ones(20.),alpha_0=5.,beta_0=5.)
+        for state in range(library_size)]
+
+####################
+#  build HDP-HSMM  #
+####################
+
+model = pyhsmm.models.HSMMIntNegBinVariant(
+        init_state_concentration=10.,
+        alpha=6.,gamma=6.,
+        obs_distns=hsmm_obs_distns,
+        dur_distns=dur_distns)
+model.trans_distn.max_likelihood([rle(labels)[0]])
+
+#############
+#  run it!  #
+#############
+
+model.add_data(data)
+model.Viterbi_EM_step()
+
