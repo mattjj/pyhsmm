7674561c9efa5dd0655d061aca7c1211904cf13f
Matthew Johnson <mattjj@csail.mit.edu>
2013-05-19 22:45:10 -0400

    fixed transpose inconsistency

--- plot.py
+++ plot.py
@@ -38,13 +38,13 @@ def pca(data,num_components=2):
 
 
 def project_data(data,vecs):
-    return np.dot(data,vecs.T)
+    return np.dot(data,vecs)
 
 
 def project_ellipsoid(ellipsoid,vecs):
     # vecs is a matrix whose columns are a subset of an orthonormal basis
     # ellipsoid is a pos def matrix
-    return np.dot(vecs,np.dot(ellipsoid,vecs.T))
+    return np.dot(vecs.T,np.dot(ellipsoid,vecs))
 
 
 def subplot_gridsize(num):
