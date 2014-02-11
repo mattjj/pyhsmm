7b51546a7c9385dd133e933269cd7a6fab94f2e3
Matthew Johnson <mattjj@csail.mit.edu>
2014-02-10 23:01:00 -0500

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,35 @@
 from distutils.core import setup
 import numpy as np
+import sys
 
 from util.cyutil import cythonize # my version of Cython.Build.cythonize
 
+# NOTE: distutils makes no sense
+
+extra_link_args = []
+extra_compile_args = []
+
+if '--with-old-clang' in sys.argv:
+    sys.argv.remove('--with-old-clang')
+    extra_compile_args.append('-stdlib=libc++')
+    extra_link_args.append('-stdlib=libc++')
+
+if '--with-openmp' in sys.argv:
+    sys.argv.remove('--with-openmp')
+    extra_compile_args.append('-fopenmp')
+    extra_link_args.append('-fopenmp')
+
+if '--with-native' in sys.argv:
+    sys.argv.remove('--with-native')
+    extra_compile_args.append('-march=native')
+
+ext_modules = cythonize('**/*.pyx')
+for e in ext_modules:
+    e.extra_compile_args.extend(extra_compile_args)
+    e.extra_link_args.extend(extra_link_args)
+
 setup(
-    ext_modules=cythonize('**/*.pyx'),
+    ext_modules=ext_modules,
     include_dirs=[np.get_include(),],
 )
 
