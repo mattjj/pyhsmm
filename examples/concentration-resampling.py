4711a8eff6f501d0042476e76b08bdd909617ca2
Matthew Johnson <mattjj@csail.mit.edu>
2014-02-10 23:37:20 -0500

    updated examples

--- examples/concentration-resampling.py
+++ examples/concentration-resampling.py
@@ -2,49 +2,31 @@ from __future__ import division
 import numpy as np
 np.seterr(divide='ignore') # these warnings are usually harmless for this code
 from matplotlib import pyplot as plt
+import os
 import scipy.stats as stats
 
 import pyhsmm
 from pyhsmm.util.text import progprint_xrange
 
-#####################
-#  data generation  #
-#####################
+###############
+#  load data  #
+###############
 
-N = 4
 T = 1000
-obs_dim = 2
-
-obs_hypparams = {'mu_0':np.zeros(obs_dim),
-                'sigma_0':np.eye(obs_dim),
-                'kappa_0':0.3,
-                'nu_0':obs_dim+5}
-
-dur_hypparams = {'alpha_0':2*30,
-                 'beta_0':2}
-
-# Construct the true observation and duration distributions
-true_obs_distns = [pyhsmm.distributions.Gaussian(**obs_hypparams) for state in range(N)]
-true_dur_distns = [pyhsmm.distributions.PoissonDuration(**dur_hypparams) for state in range(N)]
-
-# Build the true HSMM model
-truemodel = pyhsmm.models.HSMM(alpha=6.,init_state_concentration=6.,
-                               obs_distns=true_obs_distns,
-                               dur_distns=true_dur_distns)
-
-# Sample data from the true model
-data, labels = truemodel.generate(T)
-
-# Plot the truth
-plt.figure()
-truemodel.plot()
-plt.gcf().suptitle('True HSMM')
+data = np.loadtxt(os.path.join(os.path.dirname(__file__),'example-data.txt'))[:T]
 
 #########################
 #  posterior inference  #
 #########################
 
-Nmax = 25
+Nmax = 20
+obs_dim = data.shape[1]
+obs_hypparams = {'mu_0':np.zeros(obs_dim),
+                'sigma_0':np.eye(obs_dim),
+                'kappa_0':0.25,
+                'nu_0':obs_dim+2}
+dur_hypparams = {'alpha_0':2*30,
+                 'beta_0':2}
 
 obs_distns = [pyhsmm.distributions.Gaussian(**obs_hypparams) for state in range(Nmax)]
 dur_distns = [pyhsmm.distributions.PoissonDuration(**dur_hypparams) for state in range(Nmax)]
--- examples/hsmm.py
+++ examples/hsmm.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np
 np.seterr(divide='ignore') # these warnings are usually harmless for this code
 from matplotlib import pyplot as plt
-import copy
+import copy, os
 
 import pyhsmm
 from pyhsmm.util.text import progprint_xrange
@@ -16,39 +16,12 @@ the (Sticky-)HDP-HMM, but explicit duration modeling can be a big advantage for
 conditioning the prior or for discovering structure in data.
 '''
 
-#####################
-#  data generation  #
-#####################
+###############
+#  load data  #
+###############
 
-N = 4
-T = 500
-obs_dim = 2
-
-obs_hypparams = {'mu_0':np.zeros(obs_dim),
-                'sigma_0':np.eye(obs_dim),
-                'kappa_0':0.3,
-                'nu_0':obs_dim+5}
-
-dur_hypparams = {'alpha_0':2*30,
-                 'beta_0':2}
-
-# Construct the true observation and duration distributions
-true_obs_distns = [pyhsmm.distributions.Gaussian(**obs_hypparams) for state in range(N)]
-true_dur_distns = [pyhsmm.distributions.PoissonDuration(**dur_hypparams) for state in range(N)]
-
-# Build the true HSMM model
-truemodel = pyhsmm.models.HSMM(
-        alpha=6.,init_state_concentration=10.,
-        obs_distns=true_obs_distns,
-        dur_distns=true_dur_distns)
-
-# Sample data from the true model
-data, labels = truemodel.generate(T)
-
-# Plot the truth
-plt.figure()
-truemodel.plot()
-plt.gcf().suptitle('True HSMM')
+T = 1000
+data = np.loadtxt(os.path.join(os.path.dirname(__file__),'example-data.txt'))[:T]
 
 #########################
 #  posterior inference  #
@@ -57,11 +30,20 @@ plt.gcf().suptitle('True HSMM')
 # Set the weak limit truncation level
 Nmax = 25
 
+# and some hyperparameters
+obs_dim = data.shape[1]
+obs_hypparams = {'mu_0':np.zeros(obs_dim),
+                'sigma_0':np.eye(obs_dim),
+                'kappa_0':0.25,
+                'nu_0':obs_dim+2}
+dur_hypparams = {'alpha_0':2*30,
+                 'beta_0':2}
+
 obs_distns = [pyhsmm.distributions.Gaussian(**obs_hypparams) for state in range(Nmax)]
 dur_distns = [pyhsmm.distributions.PoissonDuration(**dur_hypparams) for state in range(Nmax)]
 
 posteriormodel = pyhsmm.models.WeakLimitHDPHSMM(
-        alpha=6.,gamma=6., # these can matter; better to sample over them (concentration-resampling.py)
+        alpha=6.,gamma=6., # these can matter; see concentration-resampling.py
         init_state_concentration=6., # pretty inconsequential
         obs_distns=obs_distns,
         dur_distns=dur_distns)
