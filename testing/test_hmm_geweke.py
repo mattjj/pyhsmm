0a8e7c0b5c9c2420f569ce372b91547919b69278
Matthew Johnson <mattjj@csail.mit.edu>
2014-08-27 19:36:34 -0400

    add multiple-sequence discrete geweke test

--- testing/test_hmm_geweke.py
+++ testing/test_hmm_geweke.py
@@ -26,7 +26,7 @@ def discrete_geweke_test():
     alpha = 3.
     init_state_concentration=3.
     T = 10
-    num_iter = 10000
+    num_iter = 5000
     num_checks = 10
 
     obs_distns = [distributions.Categorical(K=Nemissions,alpha_0=1.)
@@ -63,6 +63,70 @@ def discrete_geweke_test():
         i,j = np.random.choice(time_indices,replace=False,size=2)
         prior_prob_of_coassignment = (prior_stateseqs[:,i] == prior_stateseqs[:,j]).mean()
         gibbs_prob_of_coassignment = (gibbs_stateseqs[:,i] == gibbs_stateseqs[:,j]).mean()
+
+        assert np.isclose(
+                prior_prob_of_coassignment,gibbs_prob_of_coassignment,
+                rtol=0.025,atol=0.025,
+                )
+
+@attr('slow')
+@runmultiple(1)
+def discrete_geweke_multiple_seqs_test():
+    Nstates = 2
+    Nemissions = 2
+    alpha = 3.
+    init_state_concentration=3.
+    T = 10
+    num_seqs = 3
+    num_iter = 5000
+    num_checks = 10
+
+    obs_distns = [distributions.Categorical(K=Nemissions,alpha_0=1.)
+            for _ in range(Nstates)]
+
+    hmm = models.HMM(
+            alpha=alpha,init_state_concentration=init_state_concentration,
+            obs_distns=obs_distns)
+
+    # generate state sequences from the prior
+    prior_stateseqss = [[] for _ in xrange(num_seqs)]
+    for itr in xrange(num_iter):
+        hmm.resample_model() # sample parameters from the prior
+
+        for itr2 in xrange(num_seqs):
+            _, stateseq = hmm.generate(T,keep=False)
+            prior_stateseqss[itr2].append(stateseq)
+
+    prior_stateseqss = np.array(prior_stateseqss)
+    assert prior_stateseqss.shape == (num_seqs,num_iter,T)
+
+    # generate state sequences using Gibbs
+    for itr in xrange(num_seqs):
+        hmm.generate(T,keep=True)
+    assert len(hmm.states_list) == num_seqs
+
+    gibbs_stateseqss = [[] for _ in xrange(num_seqs)]
+    for itr in xrange(num_iter):
+        for s in hmm.states_list:
+            s.generate_obs() # resamples data given state sequence, obs params
+        hmm.resample_model() # resamples everything else as usual
+
+        for itr2, s in enumerate(hmm.states_list):
+            gibbs_stateseqss[itr2].append(s.stateseq)
+
+    gibbs_stateseqss = np.array(gibbs_stateseqss)
+    assert gibbs_stateseqss.shape == (num_seqs,num_iter,T)
+
+    # test that they look similar by checking probability of co-assignment
+    time_indices = np.arange(T)
+    seq_indices = np.arange(num_seqs)
+    for itr in xrange(num_checks):
+        i,j = np.random.choice(time_indices,replace=False,size=2)
+        si,sj = np.random.choice(seq_indices,replace=True,size=2)
+        prior_prob_of_coassignment = \
+                (prior_stateseqss[si,:,i] == prior_stateseqss[sj,:,j]).mean()
+        gibbs_prob_of_coassignment = \
+                (gibbs_stateseqss[si,:,i] == gibbs_stateseqss[sj,:,j]).mean()
 
         assert np.isclose(
                 prior_prob_of_coassignment,gibbs_prob_of_coassignment,
