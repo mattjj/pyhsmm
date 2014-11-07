e0817284b4127c82d3a19ff75a07b728df468014
Matthew Johnson <mattjj@csail.mit.edu>
2014-11-07 14:53:52 -0500

    re-normalize for numerical robustness

--- internals/hsmm_inb_states.py
+++ internals/hsmm_inb_states.py
@@ -442,6 +442,7 @@ class HSMMStatesTruncatedIntegerNegativeBinomial(HSMMStatesDelayedIntegerNegativ
         enters = [stats.binom.pmf(np.arange(r)[::-1],r-1,p) for A,r,p in zip(As,self.rs,self.ps)]
         norms = [sum(v.dot(np.linalg.matrix_power(A,d))[-1]*(1-p) for d in xrange(delay))
                 for A,v,p,delay in zip(As,enters,self.ps,self.delays)]
-        return [v.dot(np.linalg.matrix_power(A,self.delays[state])) / (1.-norm)
+        enters = [v.dot(np.linalg.matrix_power(A,self.delays[state])) / (1.-norm)
                 for state, (A,v,norm) in enumerate(zip(As,enters,norms))]
+        return [v / v.sum() for v in enters]
 
