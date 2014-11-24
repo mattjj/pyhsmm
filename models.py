df37998532023223f0bdfca0194090a4ef01362b
Matthew Johnson <mattjj@csail.mit.edu>
2014-11-24 14:13:28 -0500

    comment out model

--- models.py
+++ models.py
@@ -1071,10 +1071,10 @@ class HSMMPossibleChangepointsSeparateTrans(
         HSMMPossibleChangepoints):
     _states_class = hsmm_states.HSMMStatesPossibleChangepointsSeparateTrans
 
-class WeakLimitHDPHSMMPossibleChangepointsSeparateTrans(
-        _SeparateTransMixin,
-        WeakLimitHDPHSMMPossibleChangepoints):
-    _states_class = hsmm_states.HSMMStatesPossibleChangepointsSeparateTrans
+# class WeakLimitHDPHSMMPossibleChangepointsSeparateTrans(
+#         _SeparateTransMixin,
+#         WeakLimitHDPHSMMPossibleChangepoints):
+#     _states_class = hsmm_states.HSMMStatesPossibleChangepointsSeparateTrans
 
 class WeakLimitHDPHSMMIntNegBinSeparateTrans(
         _SeparateTransMixin,
