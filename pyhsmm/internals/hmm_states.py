mattjj/pyhsmm

commit 53218fb68d545f45b189aa10d287f1173de56579

replace np.logaddexp.reduce with scipy.misc.logsumexp

pyhsmm/internals/hmm_states.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy import newaxis as na
 import abc
 import copy
+from scipy.misc import logsumexp
 
 from pyhsmm.util.stats import sample_discrete
 try:
@@ -253,7 +254,7 @@ class HMMStatesPython(_StatesBase):
         betal = np.zeros_like(aBl)
 
         for t in xrange(betal.shape[0]-2,-1,-1):
-            np.logaddexp.reduce(Al + betal[t+1] + aBl[t+1],axis=1,out=betal[t])
+            logsumexp(Al + betal[t+1] + aBl[t+1],axis=1,out=betal[t])
 
         np.seterr(**errs)
         return betal
@@ -261,7 +262,7 @@ class HMMStatesPython(_StatesBase):
     def messages_backwards_log(self):
         betal = self._messages_backwards_log(self.trans_matrix,self.aBl)
         assert not np.isnan(betal).any()
-        self._normalizer = np.logaddexp.reduce(np.log(self.pi_0) + betal[0] + self.aBl[0])
+        self._normalizer = logsumexp(np.log(self.pi_0) + betal[0] + self.aBl[0])
         return betal
 
     @staticmethod
@@ -274,7 +275,7 @@ class HMMStatesPython(_StatesBase):
 
         alphal[0] = np.log(init_state_distn) + aBl[0]
         for t in xrange(alphal.shape[0]-1):
-            alphal[t+1] = np.logaddexp.reduce(alphal[t] + Al.T,axis=1) + aBl[t+1]
+            alphal[t+1] = logsumexp(alphal[t] + Al.T,axis=1) + aBl[t+1]
 
         np.seterr(**errs)
         return alphal
@@ -282,7 +283,7 @@ class HMMStatesPython(_StatesBase):
     def messages_forwards_log(self):
         alphal = self._messages_forwards_log(self.trans_matrix,self.pi_0,self.aBl)
         assert not np.any(np.isnan(alphal))
-        self._normalizer = np.logaddexp.reduce(alphal[-1])
+        self._normalizer = logsumexp(alphal[-1])
         return alphal
 
     @staticmethod
@@ -480,7 +481,7 @@ class HMMStatesPython(_StatesBase):
         joints /= joints.sum((1,2))[:,na,na] # NOTE: renormalizing each isnt really necessary
         expected_transcounts = joints.sum(0)
 
-        normalizer = np.logaddexp.reduce(alphal[0] + betal[0])
+        normalizer = logsumexp(alphal[0] + betal[0])
 
         return expected_states, expected_transcounts, normalizer
 

pyhsmm/models.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
 from matplotlib import cm
 from warnings import warn
+from scipy.misc import logsumexp
 
 import pyhsmm
 from pyhsmm.basic.abstractions import Model, ModelGibbsSampling, \
@@ -129,9 +130,9 @@ class _HMMBase(Model):
                 cmaxes = cmaxes[:-step]
                 scaled_alphal = scaled_alphal[:-step].dot(np.linalg.matrix_power(s.trans_matrix,step))
 
-                future_likelihoods = np.logaddexp.reduce(
+                future_likelihoods = logsumexp(
                         np.log(scaled_alphal) + cmaxes[:,None] + s.aBl[k:],axis=1)
-                past_likelihoods = np.logaddexp.reduce(alphal[:-k],axis=1)
+                past_likelihoods = logsumexp(alphal[:-k],axis=1)
                 outs.append(future_likelihoods - past_likelihoods)
 
                 prev_k = k
@@ -1128,9 +1129,9 @@ class HSMMIntNegBin(_HSMMGibbsSampling,_HSMMMeanField,_HSMMSVI,_HSMMViterbiEM,
             cmaxes = cmaxes[:-step]
             scaled_alphal = scaled_alphal[:-step].dot(np.linalg.matrix_power(s.hmm_trans_matrix,step))
 
-            future_likelihoods = np.logaddexp.reduce(
+            future_likelihoods = logsumexp(
                     np.log(scaled_alphal) + cmaxes[:,None] + s.hmm_aBl[k:],axis=1)
-            past_likelihoods = np.logaddexp.reduce(alphal[:-k],axis=1)
+            past_likelihoods = logsumexp(alphal[:-k],axis=1)
             outs.append(future_likelihoods - past_likelihoods)
 
             prev_k = k

pyhsmm/basic/distributions.py
@@ -1,7 +1,6 @@
 from __future__ import division
 import numpy as np
-import scipy.stats as stats
-import scipy.special as special
+from scipy.misc import logsumexp
 
 from pybasicbayes.distributions import *
 from pybasicbayes.models import MixtureDistribution
@@ -123,7 +122,7 @@ class MixtureDistribution(MixtureDistribution, DurationDistribution):
         for idx, c in enumerate(self.components):
             vals[:,idx] = c.log_sf(x)
         vals += self.weights.log_likelihood(np.arange(K))
-        return np.logaddexp.reduce(vals,axis=1)
+        return logsumexp(vals,axis=1)
 
 ##########
 #  Meta  #

pyhsmm/internals/hsmm_inb_states.py
@@ -3,6 +3,7 @@ import numpy as np
 import abc
 import scipy.stats as stats
 import scipy.special as special
+from scipy.misc import logsumexp
 
 try:
     from ..util.cstats import sample_markov
@@ -170,10 +171,10 @@ class HSMMStatesIntegerNegativeBinomial(_HSMMStatesIntegerNegativeBinomialBase):
         # betal[-1] is 0
         for t in xrange(T-1,-1,-1):
             pmess += self.hmm_aBl[t]
-            betastarl[t] = np.logaddexp.reduce(np.log(foo) + pmess, axis=1)
-            betal[t-1] = np.logaddexp.reduce(Al + betastarl[t], axis=1)
+            betastarl[t] = logsumexp(np.log(foo) + pmess, axis=1)
+            betal[t-1] = logsumexp(Al + betastarl[t], axis=1)
 
-            pmess = np.logaddexp.reduce(np.log(bar) + pmess, axis=1)
+            pmess = logsumexp(np.log(bar) + pmess, axis=1)
             pmess[ends-1] = np.logaddexp(pmess[ends-1],betal[t-1] + np.log(1-self.ps))
         betal[-1] = 0.
 

pyhsmm/internals/hsmm_states.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 from numpy import newaxis as na
+from scipy.misc import logsumexp
 
 from pyhsmm.util.stats import sample_discrete
 from pyhsmm.util.general import rle, rcumsum, cumsum
@@ -505,7 +506,7 @@ class HSMMStatesEigen(HSMMStatesPython):
         assert not np.isnan(betastarl).any()
 
         if not self.left_censoring:
-            self._normalizer = np.logaddexp.reduce(np.log(self.pi_0) + betastarl[0])
+            self._normalizer = logsumexp(np.log(self.pi_0) + betastarl[0])
         else:
             raise NotImplementedError
 
@@ -944,7 +945,7 @@ class DiagGaussGMMStates(HSMMStatesPossibleChangepointsSeparateTrans):
             all_likes += weights[na,...]
 
             # aBl is T x Nstates
-            aBl = self._aBl = np.logaddexp.reduce(all_likes, axis=2)
+            aBl = self._aBl = logsumexp(all_likes, axis=2)
             aBl[np.isnan(aBl).any(1)] = 0.
 
             aBBl = self._aBBl = np.empty((self.Tblock,self.num_states))
@@ -991,18 +992,18 @@ def hsmm_messages_backwards_log(
     for t in xrange(T-1,-1,-1):
         cB, offset = cumulative_obs_potentials(t)
         dp = dur_potentials(t)
-        np.logaddexp.reduce(betal[t:t+cB.shape[0]] + cB + dur_potentials(t),
+        logsumexp(betal[t:t+cB.shape[0]] + cB + dur_potentials(t),
                 axis=0, out=betastarl[t])
         betastarl[t] -= offset
         if right_censoring:
             np.logaddexp(betastarl[t], cB[-1] - offset + dur_survival_potentials(t),
                     out=betastarl[t])
-        np.logaddexp.reduce(betastarl[t] + trans_potentials(t-1),
+        logsumexp(betastarl[t] + trans_potentials(t-1),
                 axis=1, out=betal[t-1])
     betal[-1] = 0. # overwritten on last iteration
 
     if not left_censoring:
-        normalizer = np.logaddexp.reduce(initial_state_potential + betastarl[0])
+        normalizer = logsumexp(initial_state_potential + betastarl[0])
     else:
         raise NotImplementedError
 
@@ -1020,19 +1021,19 @@ def hsmm_messages_forwards_log(
     alphastarl[0] = initial_state_potential
     for t in xrange(T-1):
         cB = reverse_cumulative_obs_potentials(t)
-        np.logaddexp.reduce(alphastarl[t+1-cB.shape[0]:t+1] + cB + reverse_dur_potentials(t),
+        logsumexp(alphastarl[t+1-cB.shape[0]:t+1] + cB + reverse_dur_potentials(t),
                 axis=0, out=alphal[t])
         if left_censoring:
             raise NotImplementedError
-        np.logaddexp.reduce(alphal[t][:,na] + trans_potential(t),
+        logsumexp(alphal[t][:,na] + trans_potential(t),
                 axis=0, out=alphastarl[t+1])
     t = T-1
     cB = reverse_cumulative_obs_potentials(t)
-    np.logaddexp.reduce(alphastarl[t+1-cB.shape[0]:t+1] + cB + reverse_dur_potentials(t),
+    logsumexp(alphastarl[t+1-cB.shape[0]:t+1] + cB + reverse_dur_potentials(t),
             axis=0, out=alphal[t])
 
     if not right_censoring:
-        normalizer = np.logaddexp.reduce(alphal[t])
+        normalizer = logsumexp(alphal[t])
     else:
         normalizer = None # TODO
 
@@ -1058,7 +1059,7 @@ def hsmm_sample_forwards_log(
     while t < T:
         ## sample the state
         nextstate_distn_log = nextstate_unsmoothed + betastarl[t]
-        nextstate_distn = np.exp(nextstate_distn_log - np.logaddexp.reduce(nextstate_distn_log))
+        nextstate_distn = np.exp(nextstate_distn_log - logsumexp(nextstate_distn_log))
         assert nextstate_distn.sum() > 0
         state = sample_discrete(nextstate_distn)
 

pyhsmm/parallel.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import numpy as np
+from scipy.misc import logsumexp
 
 # NOTE: pass arguments through global variables instead of arguments to exploit
 # the fact that they're read-only and multiprocessing/joblib uses fork
@@ -62,10 +63,10 @@ scaled_alphal = None
 trans_matrix = None
 aBl = None
 def _get_predictive_likelihoods(k):
-    future_likelihoods = np.logaddexp.reduce(
+    future_likelihoods = logsumexp(
             np.log(scaled_alphal[:-k].dot(np.linalg.matrix_power(trans_matrix,k))) \
                     + cmaxes[:-k,None] + aBl[k:], axis=1)
-    past_likelihoods = np.logaddexp.reduce(alphal[:-k], axis=1)
+    past_likelihoods = logsumexp(alphal[:-k], axis=1)
 
     return future_likelihoods - past_likelihoods
 

commit a0de4e257ba1934200e956a8015c7e317212a646

make tests run again (that logsumexp commit 53218fb broke things!)

pyhsmm/internals/hmm_states.py
@@ -332,7 +332,7 @@ class HMMStatesPython(_StatesBase):
                 logtot += np.log(norm) + cmax
             else:
                 alphan[t:] = 0.
-                return alphan, np.log(0.)
+                return alphan, -np.inf
             in_potential = alphan[t].dot(A)
 
         return alphan, logtot

pyhsmm/models.py
@@ -10,13 +10,13 @@ from matplotlib import cm
 from warnings import warn
 from scipy.misc import logsumexp
 
-import pyhsmm
 from pyhsmm.basic.abstractions import Model, ModelGibbsSampling, \
-        ModelEM, ModelMAPEM, ModelMeanField, ModelMeanFieldSVI, ModelParallelTempering
+    ModelEM, ModelMAPEM, ModelMeanField, ModelMeanFieldSVI, ModelParallelTempering
 from pyhsmm.internals import hmm_states, hsmm_states, hsmm_inb_states, \
-        initial_state, transitions
+    initial_state, transitions
 from pyhsmm.util.general import list_split
 from pyhsmm.util.profiling import line_profiled
+from pybasicbayes.util.stats import atleast_2d
 
 ################
 #  HMM Mixins  #
@@ -83,7 +83,7 @@ class _HMMBase(Model):
         else:
             # filling in missing data
             data = s.data
-            nan_idx, = np.where(np.isnan(data).any(1))
+            nan_idx, = np.where(np.isnan(atleast_2d(data)).any(1))
             counts = np.bincount(s.stateseq[nan_idx],minlength=self.num_states)
             obs = [iter(o.rvs(count)) for o, count in zip(s.obs_distns,counts)]
             for idx, state in zip(nan_idx, s.stateseq[nan_idx]):

pyhsmm/util/testing.py
@@ -60,7 +60,7 @@ def populations_eq_quantile_plot(pop1, pop2, fig=None, percentilecutoff=5):
 
         ims1, ims2 = pop1.dot(projs.T), pop2.dot(projs.T)
         for i, (im1, im2) in enumerate(zip(ims1.T,ims2.T)):
-            plt.subplot(2,D,i)
+            plt.subplot(2,D,i+1)
             plot_1d_scaled_quantiles(im1,im2,plot_midline=firsttime)
 
 ### numerical

tests/test_hmm_geweke.py
@@ -5,9 +5,9 @@ from nose.plugins.attrib import attr
 import os
 import matplotlib.pyplot as plt
 
-import pyhsmm
 from pyhsmm import models, distributions
-from pyhsmm.util import testing
+from pybasicbayes.util import testing
+
 
 ##########
 #  util  #
@@ -35,6 +35,7 @@ def mkdir(path):
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else: raise
+
 
 figure_dir_path = os.path.join(os.path.dirname(__file__),'figures')
 mkdir(figure_dir_path)
@@ -79,8 +80,9 @@ def discrete_geweke_test(fig):
     gibbs_stateseqs = []
     gibbs_weights = []
     for itr in xrange(num_iter):
-        s.generate_obs() # resamples data given state sequence, obs params
-        hmm.resample_model() # resamples everything else as usual
+        s.data = None
+        hmm._generate_obs(s)  # resamples data given state sequence, obs params
+        hmm.resample_model()  # resamples everything else as usual
         gibbs_stateseqs.append(s.stateseq)
         gibbs_weights.append(hmm.obs_distns[0].weights)
     gibbs_stateseqs = np.array(gibbs_stateseqs)
@@ -102,6 +104,7 @@ def discrete_geweke_test(fig):
     testing.populations_eq_quantile_plot(prior_weights,gibbs_weights,fig=fig)
     figpath = os.path.join(figure_dir_path,'discrete_geweke_test_weights.pdf')
     plt.savefig(figpath)
+
 
 @attr('slow')
 @runmultiple(2)
@@ -147,8 +150,9 @@ def discrete_geweke_multiple_seqs_test(fig):
     gibbs_weights = []
     for itr in xrange(num_iter):
         for s in hmm.states_list:
-            s.generate_obs() # resamples data given state sequence, obs params
-        hmm.resample_model() # resamples everything else as usual
+            s.data = None
+            hmm._generate_obs(s)  # resamples data given state sequence, obs params
+        hmm.resample_model()  # resamples everything else as usual
 
         for itr2, s in enumerate(hmm.states_list):
             gibbs_stateseqss[itr2].append(s.stateseq)