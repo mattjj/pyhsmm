152a064be31232aa60f6959e2637039701517526
Matthew Johnson <mattjj@csail.mit.edu>
2015-10-20 13:45:09 -0400

    update setup.py using my style from autograd, i think it works

--- setup.py
+++ setup.py
@@ -1,108 +1,65 @@
-from distutils.core import setup, Extension
-import numpy as np
-import sys
-import os
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+from distutils.errors import CompileError
+from warnings import warn
+import os.path
 from glob import glob
 
-PYHSMM_VERSION = "0.1.4"
-
-###########################
-#  compilation arguments  #
-###########################
-
-extra_link_args = []
-extra_compile_args = []
-
-if '--with-old-clang' in sys.argv:
-    sys.argv.remove('--with-old-clang')
-    extra_compile_args.append('-stdlib=libc++')
-    extra_link_args.append('-stdlib=libc++')
-
-if '--with-openmp' in sys.argv:
-    sys.argv.remove('--with-openmp')
-    extra_compile_args.append('-fopenmp')
-    extra_link_args.append('-fopenmp')
-
-if '--with-native' in sys.argv:
-    sys.argv.remove('--with-native')
-    extra_compile_args.append('-march=native')
-
-if '--with-mkl' in sys.argv:
-    sys.argv.remove('--with-mkl')
-    # NOTE: there's no way this will work on Windows
-    extra_compile_args.extend(['-m64','-I' + os.environ['MKLROOT'] + '/include','-DEIGEN_USE_MKL_ALL'])
-    extra_link_args.extend(('-Wl,--start-group %(MKLROOT)s/lib/intel64/libmkl_intel_lp64.a %(MKLROOT)s/lib/intel64/libmkl_core.a %(MKLROOT)s/lib/intel64/libmkl_sequential.a -Wl,--end-group -lm' % {'MKLROOT':os.environ['MKLROOT']}).split(' '))
-
-if '--with-assembly' in sys.argv:
-    sys.argv.remove('--with-assembly')
-    extra_compile_args.extend(['--save-temps','-masm=intel','-fverbose-asm'])
-
-if '--with-cython' in sys.argv:
-    sys.argv.remove('--with-cython')
-    use_cython = True
-else:
+try:
+    from Cython.Distutils import build_ext as _build_ext
+except ImportError:
     use_cython = False
+else:
+    use_cython = True
 
+class build_ext(_build_ext):
+    # see http://stackoverflow.com/q/19919905 for explanation
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy as np
+        self.include_dirs.append(np.get_include())
 
-#######################
-#  extension modules  #
-#######################
+    # if extension modules fail to build, keep going anyway
+    def run(self):
+        try:
+            _build_ext.run(self)
+        except CompileError:
+            warn('Failed to build extension modules')
 
-cython_pathspec = os.path.join('pyhsmm','**','*.pyx')
-
+extension_pathspec = os.path.join('pyhsmm','**','*.pyx')
 if use_cython:
     from Cython.Build import cythonize
-    ext_modules = cythonize(cython_pathspec)
+    ext_modules = cythonize(extension_pathspec)
 else:
-    paths = [os.path.splitext(fp)[0] for fp in glob(cython_pathspec)]
+    paths = [os.path.splitext(fp)[0] for fp in glob(extension_pathspec)]
     names = ['.'.join(os.path.split(p)) for p in paths]
     ext_modules = [
-        Extension(name,
-                  sources=[path + '.cpp'],
-                  include_dirs=[os.path.join('pyhsmm','deps','Eigen3')],
-                  extra_compile_args=['-O3','-std=c++11','-DNDEBUG','-w',
-                                      '-DHMM_TEMPS_ON_HEAP'])
+        Extension(
+            name, sources=[path + '.cpp'],
+            include_dirs=[os.path.join('pyhsmm','deps','Eigen3')],
+            extra_compile_args=['-O3','-std=c++11','-DNDEBUG','-w','-DHMM_TEMPS_ON_HEAP'])
         for name, path in zip(names,paths)]
 
-for e in ext_modules:
-    e.extra_compile_args.extend(extra_compile_args)
-    e.extra_link_args.extend(extra_link_args)
-
-
-############
-#  basics  #
-############
-
 setup(name='pyhsmm',
-      version=PYHSMM_VERSION,
+      version='0.1.5',
       description="Bayesian inference in HSMMs and HMMs",
       author='Matthew James Johnson',
       author_email='mattjj@csail.mit.edu',
-      maintainer='Matthew James Johnson',
-      maintainer_email='mattjj@csail.mit.edu',
       url="https://github.com/mattjj/pyhsmm",
       license='MIT',
-      packages=['pyhsmm',
-                'pyhsmm.basic',
-                'pyhsmm.internals',
-                'pyhsmm.plugins',
-                'pyhsmm.util'],
+      packages=['pyhsmm', 'pyhsmm.basic', 'pyhsmm.internals', 'pyhsmm.util'],
       platforms='ALL',
-      keywords=['bayesian', 'inference', 'mcmc', 'time-series',
-                'monte-carlo'],
+      keywords=['bayesian', 'inference', 'mcmc', 'time-series', 'monte-carlo'],
       install_requires=[
-          "Cython >= 0.20.1",
-          "numpy",
-          "scipy",
-          "matplotlib",
-          "nose",
-          "pybasicbayes >= 0.1.3",
-      ],
+          "Cython >= 0.20.1", "numpy", "scipy", "matplotlib",
+          "nose", "pybasicbayes >= 0.1.3", ],
+      setup_requires=['numpy'],
       package_data={"pyhsmm": [os.path.join("examples", "*.txt")]},
       ext_modules=ext_modules,
-      include_dirs=[np.get_include(),],
       classifiers=[
+          'Development Status :: 4 - Beta',
           'Intended Audience :: Science/Research',
           'Programming Language :: Python',
-          'Programming Language :: C++',
-      ])
+          'Programming Language :: C++'],
+      cmdclass={'build_ext': build_ext})
